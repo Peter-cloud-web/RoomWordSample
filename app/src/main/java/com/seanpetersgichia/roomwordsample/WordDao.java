69bb8d047cb4f677a1df557a05c7951be172503b
Peter-cloud-web <petergichia165@gmail.com>
2020-08-15 05:58:51 +0300

    provide the insert body less method and annotate it with insert anntation to declare method to insert a word and conflict startegy to ignore a word if its inserted 2 times

--- app/src/main/java/com/seanpetersgichia/roomwordsample/WordDao.java
+++ app/src/main/java/com/seanpetersgichia/roomwordsample/WordDao.java
@@ -1,7 +1,11 @@
 package com.seanpetersgichia.roomwordsample;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 
 @Dao
 public interface WordDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
 }
