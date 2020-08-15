d251a86fd12f4e7b01085b4eae82e5ad1b694fbf
Peter-cloud-web <petergichia165@gmail.com>
2020-08-15 06:00:59 +0300

    declare a method to delete all words

--- app/src/main/java/com/seanpetersgichia/roomwordsample/WordDao.java
+++ app/src/main/java/com/seanpetersgichia/roomwordsample/WordDao.java
@@ -3,9 +3,13 @@ package com.seanpetersgichia.roomwordsample;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 @Dao
 public interface WordDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
 }
