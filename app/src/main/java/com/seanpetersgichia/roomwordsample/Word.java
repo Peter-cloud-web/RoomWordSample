479f9772da15242207d5b969d9c90fa099f3a9a1
Peter-cloud-web <petergichia165@gmail.com>
2020-08-15 05:25:51 +0300

    provide required fields for the class which will represent a column in the table

--- app/src/main/java/com/seanpetersgichia/roomwordsample/Word.java
+++ app/src/main/java/com/seanpetersgichia/roomwordsample/Word.java
@@ -1,4 +1,18 @@
 package com.seanpetersgichia.roomwordsample;
 
+import androidx.annotation.NonNull;
+
 public class Word {
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    public String getWord(){
+        return this.mWord;
+    }
+
+
+
 }
