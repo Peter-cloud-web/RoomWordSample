eecf63c8356b5a772852203a2a91e4d97ca87b68
Peter-cloud-web <petergichia165@gmail.com>
2020-08-16 03:53:03 +0300

    calling constructor from the superclass and passing in application as parameter

--- app/src/main/java/com/seanpetersgichia/roomwordsample/WordViewModel.java
+++ app/src/main/java/com/seanpetersgichia/roomwordsample/WordViewModel.java
@@ -1,4 +1,19 @@
 package com.seanpetersgichia.roomwordsample;
 
-public class WordViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application){
+        super(application);
+    }
 }
