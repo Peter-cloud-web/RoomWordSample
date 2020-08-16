0b73933a37215eb8679c11c3a773f732d9ac2f62
Peter-cloud-web <petergichia165@gmail.com>
2020-08-16 05:07:05 +0300

    Insert words to the database

--- app/src/main/java/com/seanpetersgichia/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/seanpetersgichia/roomwordsample/WordRoomDatabase.java
@@ -2,9 +2,11 @@ package com.seanpetersgichia.roomwordsample;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,6 +30,24 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     }
 
     public abstract WordDao wordDao();
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+
+        public void onOpen(@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+
+            databaseWriteExecutor.execute(() -> {
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Quarantine");
+                dao.insert(word);
+                word = new Word("Kenya");
+                dao.insert(word);
+            });
+        }
+
+    }
 
 
 }
