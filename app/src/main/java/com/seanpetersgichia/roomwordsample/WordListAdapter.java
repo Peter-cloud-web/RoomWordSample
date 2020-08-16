1e89b5d4eeb734a1db263d2490a08abf49b2c71c
Peter-cloud-web <petergichia165@gmail.com>
2020-08-16 04:38:58 +0300

    Implement the adapter to provide data to the view model

---
+++ app/src/main/java/com/seanpetersgichia/roomwordsample/WordListAdapter.java
@@ -0,0 +1,24 @@
+package com.seanpetersgichia.roomwordsample;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordListViewHolder> {
+    @NonNull
+    @Override
+    public WordListAdapter.WordListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
